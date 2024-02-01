aabf485a28fc89f6cecc1df9b61e63429b8a587a
mrdevsam <mdshoykotislam05@gmail.com>
2024-02-01 20:13:03 +0600

    adding fifth array related java program

---
+++ JavaPrograms/JavaArrayProblemFive.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class JavaArrayProblemFive {
+	// find the largest element of an array
+	public static void main(String[] args) {
+		double[] arr = {45.4, -23.70, 25.65, 85.98, -99.67};
+		double largest = arr[0];
+
+		for(double num : arr) {
+			if(largest < num) {
+				largest = num;
+			}
+		}
+
+		System.out.format("Largest element: %.2f",largest);
+	}
+}

2febbb7595ee8f834283f54ad7b6df90ee2537c3
mrdevsam <mdshoykotislam05@gmail.com>
2024-02-01 16:27:02 +0600

    adding second array related java program

---
+++ JavaPrograms/JavaArrayProblemTwo.java
@@ -0,0 +1,13 @@
+public class JavaArrayProblemTwo {
+	// getting the sum of all elements in a array
+	public static void main(String[] args) {
+		int[] my_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int sum = 0;
+		
+		for(int n : my_array) {
+			sum += n;
+		}
+		
+		System.out.println("The sum is: " + sum);
+	}
+}

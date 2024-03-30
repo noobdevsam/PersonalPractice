53a8f45a30bb4b2f527c87366bec69595225d45c
mrdevsam <mdshoykotislam05@gmail.com>
2024-03-31 00:46:41 +0600

    check if a element exists or not

--- JavaPrograms/arrays/ArrayThree.java
+++ JavaPrograms/arrays/ArrayThree.java
@@ -1,5 +1,22 @@
 package arrays;
 
+import java.util.Arrays;
+
+// check if a value exists in a array
 public class ArrayThree {
-    
+    public static void main(String[] args) {
+        int[] arr = {54,78,235,65,45,323};
+        System.out.println(ispresent(arr,43));
+        System.out.println(ispresent(arr,65));
+    }
+
+    private static boolean ispresent(int[] arr, int i) {
+        for (int n : arr) {
+            if (i == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
